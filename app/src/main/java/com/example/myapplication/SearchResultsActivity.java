d03d8fad5772a270f19bdef966de5039976517ea
main computer <rcahgollabonilla@gmail.edu>
2023-05-13 17:12:33 -0700

    added styleable toasts.

--- app/src/main/java/com/example/myapplication/SearchResultsActivity.java
+++ app/src/main/java/com/example/myapplication/SearchResultsActivity.java
@@ -7,7 +7,9 @@ import androidx.room.Room;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,6 +21,8 @@ import com.example.myapplication.DB.UserDAO;
 import com.example.myapplication.databinding.ActivitySearchResultsBinding;
 
 import java.util.List;
+
+import io.github.muddz.styleabletoast.StyleableToast;
 
 public class SearchResultsActivity extends AppCompatActivity implements RecyclerViewInterface{
     List<Flights> mFlights;
@@ -112,10 +116,22 @@ public class SearchResultsActivity extends AppCompatActivity implements Recycler
         int points = mQuantity * 150;
         int capacity = mFlights.get(position).getCapacity();
         if(purchases > capacity){
-            Toast.makeText(SearchResultsActivity.this, "Flight Can't Be Booked!", Toast.LENGTH_SHORT).show();
+            new StyleableToast
+                    .Builder(SearchResultsActivity.this)
+                    .text("Flight Can't Be Booked!")
+                    .textColor(Color.WHITE)
+                    .gravity(Gravity.TOP)
+                    .backgroundColor(Color.RED)
+                    .show();
             return;
         }
-        Toast.makeText(SearchResultsActivity.this, "Flight Booked!", Toast.LENGTH_SHORT).show();
+        new StyleableToast
+                .Builder(SearchResultsActivity.this)
+                .text("Flight Booked!")
+                .textColor(Color.WHITE)
+                .gravity(Gravity.TOP)
+                .backgroundColor(Color.GREEN)
+                .show();
         Bookings booking = new Bookings(mUserID, mFlights.get(position).getFlightId(), mQuantity);
         mBookingsDAO.insert(booking);
         mFlightsDAO.setPurchases(mFlights.get(position).getFlightId(), purchases);
